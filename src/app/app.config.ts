b97651c37d55fc041e899c7f1f079ce6015ba4f5
swwayam <codingswayam24@gmail.com>
2023-11-22 17:14:35 +0530

    firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {provideFirebase} from '@angular/fire/app'
 
 import { routes } from './app.routes';
 
---
+++ src/app/sign-in/sign-in.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sign-in',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sign-in.component.html',
+  styleUrl: './sign-in.component.css'
+})
+export class SignInComponent {
+
+}
